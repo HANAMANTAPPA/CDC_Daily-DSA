e3924cb9d2ce4879f1afc43f9268ed90f03e9edb
Hanumanth <48180907+HANAMANTAPPA@users.noreply.github.com>
2022-09-07 02:04:29 +0530

    Update Remove the white space from sentence.java

--- Remove the white space from sentence.java
+++ Remove the white space from sentence.java
@@ -1,1 +1,48 @@
+/*
+Given a String with white spaces, the task is to remove all white spaces from a string
+
+Input:str = "     ha ha     !!   done by       hanumanth           ";
+output:  haha!!donebyhanumanth
+
+
+
+
+
+*/
+public class MyClass {
+    public static void main(String args[]) {
+      
+      String str = "     ha ha     !!   done by       hanumanth           ";
+
+      System.out.println( removespaces(str) );
+      System.out.println( "------Builtin fun------" );
+      System.out.println( removespacesBUIltinfun(str) );
+    }
+    
+    public static String removespaces(String s){
+        String str="";
+        for(int i=0;i<s.length() ;i++ ){
+            if( s.charAt(i) != ' ' ){
+                str+=s.charAt(i);
+            }
+        }
+        return str;
+    }
+    
+    public static String removespacesBUIltinfun(String s){
+        String str = s.replaceAll("\\s", "");
+        return str;
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
 
